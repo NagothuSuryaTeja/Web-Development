b372d1621cf9783a424e6bbe344b2c4ce36acf8a
NagothuSuryaTeja <nagothuteja@gmail.com>
2025-01-23 21:06:47 +0530

    23/01/2024 New Files Uploded..

--- Web Developement Projects/Projects/Simon Game Challenge Starting Files/script.js
+++ Web Developement Projects/Projects/Simon Game Challenge Starting Files/script.js
@@ -1,85 +1,39 @@
-var colors=["red","green","blue","yellow"];
-var pcGamePattern=[];
-var userGamePattern=[];
-$(document).ready(function(){
-   
-    $(".red").on("click",function()
-    { 
-        var redaudio= new Audio("./sounds/red.mp3");
-        redaudio.play();
-        userGamePattern.push("red");
-    });
-    $(".blue").on("click",function()
-    { 
-        var redaudio= new Audio("./sounds/blue.mp3");
-        redaudio.play();
-        userGamePattern.push("bule");
-    });
-    $(".yellow").on("click",function()
-    { 
-        var redaudio= new Audio("./sounds/yellow.mp3");
-        redaudio.play();
-        userGamePattern.push("yellow");
-    });
-    $(".green").on("click",function()
-    { 
-
-        var redaudio= new Audio("./sounds/green.mp3");
-        redaudio.play();
-        userGamePattern.push("green");
-    });
-
-    function nextSequences(){
-        var index=Math.floor(Math.random()*3);
-        var color=colors[index];
-        pcGamePattern.push(color);
-        switch(color)
+$(document).ready(function()
+{
+    var started=true;
+    var level =1;
+    var colors=["red","blue","green","yellow"];
+    var computerInput=[];
+    var userInput=[];
+    $(document).on("keypress",function(event)
+    {
+        if(started)
         {
-            case "red":
-                var redaudio= new Audio("./sounds/red.mp3");
-                redaudio.play();
-            case "blue":
-                var redaudio= new Audio("./sounds/blue.mp3");
-                redaudio.play();
-            case "yellow":    
-                var redaudio= new Audio("./sounds/yellow.mp3");
-                redaudio.play();
-            case "green":
-                var redaudio= new Audio("./sounds/green.mp3");
-                redaudio.play();
-            
+            started=false;
+            $("h1").text("level "+level);
+            alert("game started");
         }
-    }
-    function validation()
-    {
-        res = pcGamePattern.join() == userGamePattern.join();
-        return res;
-    }
-    $(document).on("click",function()
-    {
-        var val=1;
-        $("h1").text("Level "+val);
-        nextSequences();
-        valid=validation();
-        if(valid)
-            {
-                alert(val++);
-            } 
-            else{
-               alert(val=0); 
-            }       
+        
     });
     
+    function pcInputSequence()
+    {
+        var index=Math.floor(Math.random()*4);
+        var randomColor=colors[index];
+        computerInput.push(randomColor);
+    }
+    function userSequence()
+    {
+        $(".btn").on("click",function(){
+            var btnColor= this.innerHTML;
+            switch(btnColor)
+            {
+                case ".red" :
+                    var redAudio = new Audio("./sounds/red.mp3");
+                    redAudio.play();
+                    userInput.push("red");
 
-
-
-
-
-
-
-
-
-
-
-    
-}); 
+            }
+        });
+    }
+});
