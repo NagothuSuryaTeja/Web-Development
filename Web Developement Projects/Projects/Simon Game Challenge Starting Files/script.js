e3b7ff19f81380148783bb07c9eecc66cd5a981e
NagothuSuryaTeja <nagothuteja@gmail.com>
2025-01-24 21:03:11 +0530

    24/01/2025 project is improved litle bit

--- Web Developement Projects/Projects/Simon Game Challenge Starting Files/script.js
+++ Web Developement Projects/Projects/Simon Game Challenge Starting Files/script.js
@@ -1,39 +1,69 @@
-$(document).ready(function()
+$(document).ready(function(){
+var selected=false;
+var colorsArray=["red","yellow","green","blue"];
+var userInputArray=[];
+var computerInputArray=[];
+var level=0;
+$(document).keypress(function(){
+  if(!selected)
+  {
+    $("h1").text("Level"+level);
+    selected=true;
+    ComputerInputFunction();
+  }
+});
+function userInputFunction(btnColor)
 {
-    var started=true;
-    var level =1;
-    var colors=["red","blue","green","yellow"];
-    var computerInput=[];
-    var userInput=[];
-    $(document).on("keypress",function(event)
-    {
-        if(started)
-        {
-            started=false;
-            $("h1").text("level "+level);
-            alert("game started");
-        }
-        
-    });
-    
-    function pcInputSequence()
-    {
-        var index=Math.floor(Math.random()*4);
-        var randomColor=colors[index];
-        computerInput.push(randomColor);
-    }
-    function userSequence()
-    {
-        $(".btn").on("click",function(){
-            var btnColor= this.innerHTML;
-            switch(btnColor)
-            {
-                case ".red" :
-                    var redAudio = new Audio("./sounds/red.mp3");
-                    redAudio.play();
-                    userInput.push("red");
+  $("."+btnColor).click(function(){
+    buttonColor= $("."+btnColor).attr("id");
+    userInputArray.push(buttonColor);
+    playSound(buttonColor);
+    buttonAnimation(buttonColor);
+  });
+}
+function ComputerInputFunction()
+{
+  level++;
+  var randomNumber=Math.floor(Math.random()*4);
+  var randomColor=colorsArray[randomNumber];
+  computerInputArray.push(randomColor);
+  playSound(randomColor);
+  buttonAnimation(randomColor);
+}
+function playSound(colorName)
+{
+  var colorSound = new Audio("./sounds/"+colorName+".mp3");
+  colorSound.play();
+}
+function buttonAnimation(colorId)
+{
+  $("#"+colorId).addClass("pressed");
+setTimeout(function(){
+    $("#"+colorId).removeClass("pressed");
+  },100);
+}
 
-            }
-        });
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
