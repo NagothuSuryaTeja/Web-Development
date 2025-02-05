41e9d0a8cb6203f13c2e5c97a8578b146a481f6e
NagothuSuryaTeja <nagothuteja@gmail.com>
2025-02-06 04:57:01 +0530

    06/02/2025 course update....

--- Web Developement Projects/Projects/Back-End/axios2/index.js
+++ Web Developement Projects/Projects/Back-End/axios2/index.js
@@ -2,12 +2,9 @@ import express from "express";
 import axios from "axios";
 const app = express();
 const port=3000;
-app.get("/", async (req,res)=>{
-    const response = await axios.get("https://api.wheretheiss.at/v1/satellites");
-    res.render("index.js",{name:response.name});
-});
-
-app.post("/",(req,res)=>{
+app.get("/", async (req,res) => {
+    const response = await axios.get("https://api.wheretheiss.at/v1/satellites/25544");
+    console.log(response.data);
     
 }); 
 app.listen(port,()=>{
